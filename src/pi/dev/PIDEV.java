bfb469f2d19e8ce7a64fe5b01b84822ef3f7af5a
AmirGattoussi <Amir.GattoussiLahouel@esprit.tn>
2023-02-14 09:33:15 +0100

    Singleton connection

---
+++ src/pi/dev/PIDEV.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pi.dev;
+import java.io.IOException;
+import java.sql.*;
+import Utils.DBconnexion;
+/**
+ *
+ * @author gtsia
+ */
+public class PIDEV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException, IOException {
+        
+      DBconnexion database = DBconnexion.getInstance();
+Connection connection = database.getConnection();
+
+        // TODO code application logic here
+    }
+    
+}
