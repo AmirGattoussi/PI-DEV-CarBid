5b1ee949ea31c6127dc8fd39113fc64dc0eca53b
AmirGattoussi <Amir.GattoussiLahouel@esprit.tn>
2023-02-14 20:26:59 +0100

    Packages Setup

--- src/Dao/EmployeeDaoImplementation.java
+++ src/Dao/EmployeeDaoImplementation.java
@@ -3,34 +3,36 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pi.dev;
+package Dao;
 
 import java.sql.*;
+import Services.*;
+import Utils.DBconnexion;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 /**
  *
  * @author gtsia
  */
-public class test_add {
-      public ServiceAbonne() {
-        cnx = MaConnexion.getInstance().getConnection();
+public class EmployeeDaoImplementation implements EmployeeDao{
+      
+    Connection cnx;
+
+    public EmployeeDaoImplementation()throws SQLException {
+        cnx = DBconnexion.getInstance().getConnection();
     }
     @Override
-    public void AddAbonne() {
-        name="name";
-    }
+    public void addEmployee(){
         try {
             Statement stm = cnx.createStatement();
-            String query = "INSERT INTO `employee`( `id`, `name`) VALUES ('" + 2 + "','" + name"')";
+            String query = "INSERT INTO employee (id, name) VALUES ( '2', 'yosr')";
             stm.executeUpdate(query);
-        } catch (SQLException ex) {
-            Logger.getLogger(ServiceAbonne.class.getName()).log(Level.SEVERE, null, ex);
+        }  catch (SQLException ex) {
+            Logger.getLogger(EmployeeDaoImplementation.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
-
+}
     /*@Override
      public void ModifierAbonne(Abonne a) throws SQLException
     { 
@@ -42,4 +44,4 @@ public class test_add {
     
     
     
-}
+}*/
---
+++ src/Services/EmployeeDao.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+/**
+ *
+ * @author gtsia
+ */
+public interface EmployeeDao {
+    public void addEmployee();
+}
