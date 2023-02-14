56f3ab2654a909db5b76f1d8fae15c3ffb031fbb
AmirGattoussi <Amir.GattoussiLahouel@esprit.tn>
2023-02-14 20:25:55 +0100

    Setup packages

---
+++ src/Main/Main.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+import java.io.IOException;
+import java.sql.*;
+import Utils.DBconnexion;
+import Dao.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author gtsia
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException, IOException {
+        
+      DBconnexion database = DBconnexion.getInstance();
+Connection connection = database.getConnection();
+EmployeeDaoImplementation e= new EmployeeDaoImplementation();
+e.addEmployee();
+
+/*String  name="name";
+    
+        try {
+            Statement stm = connection.createStatement();
+            String query ="INSERT INTO employee (id, name) VALUES ( '1', 'name')";
+            stm.executeUpdate(query);
+        } catch (SQLException ex) {
+            System.out.println("failed");
+        }*/
+//first commit
+        // TODO code application logic here
+        
+    }
+    
+}   
