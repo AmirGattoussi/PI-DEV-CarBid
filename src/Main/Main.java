af9ca871ed49c198043a6f69a1a5dc1c4be4852f
yosr01 <62879131+yosr01@users.noreply.github.com>
2023-02-18 11:56:25 +0100

    test commit

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -22,7 +22,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws SQLException, IOException {
-        System.out.println("yosr moalla");
+        System.out.println("New clone");
     }
 
 }
