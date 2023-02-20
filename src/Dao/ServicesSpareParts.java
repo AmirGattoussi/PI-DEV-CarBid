6ed4d1d6862296e9bd56434347968542210a0028
AmirGattoussi <Amir.GattoussiLahouel@esprit.tn>
2023-02-20 14:06:35 +0100

    fix

--- src/Dao/ServicesSpareParts.java
+++ src/Dao/ServicesSpareParts.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import Utils.DBconnexion;
+
 /*
 import entities.SpareParts;
 import java.sql.ResultSet;
@@ -30,72 +31,70 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import tools.MyConnection;
-*/
-
+ */
 
 /**
  *
  * @author Yasmine
- * 
+ *
  * public class ReservationDao implements IReservationDao{
-
-    Connection cnx;
-
-    public ReservationDao()throws SQLException {
-        cnx = DBconnexion.getInstance().getConnection();
-    }
+ *
+ * Connection cnx;
+ *
+ * public ReservationDao()throws SQLException { cnx =
+ * DBconnexion.getInstance().getConnection(); }
  */
-public class ServicesSpareParts  {
+public class ServicesSpareParts {
+
     Connection connection;
     Statement ste;
 
     Connection cnx;
 
-    public ServicesSpareParts()throws SQLException {
+    public ServicesSpareParts() throws SQLException {
         cnx = DBconnexion.getInstance().getConnection();
     }
+
     public ArrayList<SpareParts> display() {
         ArrayList<SpareParts> listpieces = new ArrayList<>();
-        try{
-        ste= connection.createStatement();
-        String req_select="SELECT * FROM `pidev`.`spareparts`";
-        ResultSet res = ste.executeQuery(req_select);
-        while(res.next()){
-            int id_sparepart = res.getInt(1);
-            String Type = res.getString(2);
-            int Pou = res.getInt(3);
-            String Description = res.getString(4);
-            double Price = res.getDouble(5);
-            String Typec = res.getString(6);
-         //   SpareParts s = new SpareParts(Id,Type,Pou,Description,Price,Typec);
-         SpareParts s = new SpareParts(id_sparepart, Type,Pou, Description,  Price, Typec);
-            listpieces.add(s);
+        try {
+            ste = connection.createStatement();
+            String req_select = "SELECT * FROM `pidev`.`spareparts`";
+            ResultSet res = ste.executeQuery(req_select);
+            while (res.next()) {
+                int id_sparepart = res.getInt(1);
+                String Type = res.getString(2);
+                int Pou = res.getInt(3);
+                String Description = res.getString(4);
+                double Price = res.getDouble(5);
+                String Typec = res.getString(6);
+                //   SpareParts s = new SpareParts(Id,Type,Pou,Description,Price,Typec);
+                SpareParts s = new SpareParts(id_sparepart, Type, Pou, Description, Price, Typec);
+                listpieces.add(s);
+            }
+        } catch (SQLException ex) {
+            System.out.println("SQLException " + ex.getMessage());
         }
-        }catch(SQLException ex){
-            System.out.println("SQLException "+ex.getMessage());
-        }
-        
+
         return listpieces;
     }
-   
-   
-    
+
     public void add(SpareParts u) throws SQLException {
         PreparedStatement pre = connection.prepareStatement("INSERT INTO `carbid`.`spareparts` (`id_sparepart`,`Type`,`Pou`,`Description`,`Price`,`Typec`) VALUES (?,?,?,?,?,?)");
 
         pre.setInt(1, u.getId());
-          pre.setString(2, u.getType());
-           pre.setInt(3, u.getPou());
-            pre.setString(4, u.getDescription());
+        pre.setString(2, u.getType());
+        pre.setInt(3, u.getPou());
+        pre.setString(4, u.getDescription());
         pre.setDouble(5, u.getPrice());
-                 
+
         pre.setString(6, u.getTypec());
 
         pre.executeUpdate();
 
     }
-    
-     public void delete(int id) {
+
+    public void delete(int id) {
 
         try {
             PreparedStatement pre = connection.prepareStatement("delete from carbid where id_sparepart = ?");
@@ -106,8 +105,8 @@ public class ServicesSpareParts  {
         }
 
     }
-     
-      public void modify(SpareParts u) {
+
+    public void modify(SpareParts u) {
 
         try {
             PreparedStatement pre = connection.prepareStatement("Update spareparts set Type=?,Pou=?,Description=?,Price=?,Typec=? where id_sparepart = ?");
@@ -129,19 +128,17 @@ public class ServicesSpareParts  {
             System.out.print(ex.getMessage());
         }
     }
-      
-       public SpareParts GetSparePartsById(int id) {
-    
+
+    public SpareParts GetSparePartsById(int id) {
 
         try {
-            
-     PreparedStatement pre = connection.prepareStatement("select * from carbid where id_sparepart = ?");
 
- 
+            PreparedStatement pre = connection.prepareStatement("select * from carbid where id_sparepart = ?");
+
             pre.setInt(1, id);
             ResultSet result = pre.executeQuery();
             while (result.next()) {
-               
+
                 SpareParts u = new SpareParts(result.getInt(1), result.getString(2), result.getInt(3), result.getString(4), result.getDouble(5), result.getString(6));
                 return u;
             }
@@ -150,8 +147,5 @@ public class ServicesSpareParts  {
         }
         return null;
     }
-      
-      
-     
+
 }
-    
