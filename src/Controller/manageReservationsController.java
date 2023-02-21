e020c36efc68f3980b5da60a215ceda6be81247f
Neil Monastiri <neil.monastiri@esprit.tn>
2023-02-21 15:08:55 +0100

    cleaned code: removed unnecessary comments and imports

--- src/Controller/manageReservationsController.java
+++ src/Controller/manageReservationsController.java
@@ -14,6 +14,11 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+/**
+ *
+ * @author neil
+ */
+
 public class manageReservationsController implements Initializable {
 
     @FXML
--- src/Services/IReservationDao.java
+++ src/Services/IReservationDao.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Services;
 
 import java.util.List;
-
-// import java.sql.Date;
-
 import Entities.*;
 
 /**
