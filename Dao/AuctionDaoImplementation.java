4ae3057cdc1309244a601cd36b5982ac287ab4c3
yasminemar <80041494+yasminemar@users.noreply.github.com>
2023-02-25 19:15:00 +0100

    Add files via upload

---
+++ Dao/AuctionDaoImplementation.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import Entities.Auction;
+import Entities.User;
+import Services.AuctionDao;
+import Utils.DBconnexion;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author asus
+ */
+public class AuctionDaoImplementation implements AuctionDao {
+    Connection cnx;
+
+    public AuctionDaoImplementation() throws SQLException {
+        cnx = DBconnexion.getInstance().getConnection();
+    }
+
+    @Override
+ public void addAuction(Auction auction) {
+        PreparedStatement statement;
+    try {
+        statement = cnx.prepareStatement(
+                "INSERT INTO auction (startDate, endDate, startingPrice,highestBid,status,idCar) VALUES (?, ?, ?, ?, ?, ?)");
+     statement.setDate(1, auction.getStartDate());
+        statement.setDate(2, auction.getEndDate());
+        statement.setFloat(3, auction.getStartingPrice());
+	statement.setFloat(4, auction.getHighestBid());
+	statement.setString(5, auction.getStatus());
+        statement.setInt(6, auction.getIdCar());
+	statement.executeUpdate();
+	System.out.println("added successfully");
+    
+    
+    } catch (SQLException ex) {
+        Logger.getLogger(AuctionDaoImplementation.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+      
+    }
+
+@Override
+public Auction getAuction(int id)  {
+        PreparedStatement statement;
+    try {
+        statement = cnx.prepareStatement(
+                "SELECT * FROM auction WHERE idAuction = ?");
+                statement.setInt(1, id);
+
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            return new Auction(
+                    resultSet.getInt("idAuction"),
+                    resultSet.getDate("startDate"),
+                    resultSet.getDate("endDate"),
+                    resultSet.getFloat("startingPrice"),
+		    resultSet.getFloat("highestBid"),
+                    resultSet.getString("status"),
+                    resultSet.getInt("idCar")
+                    
+            );
+            
+        }
+
+        return null;
+    } catch (SQLException ex) {
+        Logger.getLogger(AuctionDaoImplementation.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    return null;
+
+
+    }
+
+@Override
+ public void updateAuction(int id, float highestBid,String status) {
+        PreparedStatement statement;
+    try {
+        statement = cnx.prepareStatement(
+                "UPDATE auction SET  highestBid= ?, status = ? WHERE idAuction = ?");
+        statement.setFloat(1, highestBid);
+        statement.setString(2, status);
+        statement.setInt(3, id);
+	statement.executeUpdate();
+	System.out.println("updated successfully");
+    } catch (SQLException ex) {
+        Logger.getLogger(AuctionDaoImplementation.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+        
+    
+ 
+ }
+
+@Override
+public void deleteAuction(int id) {
+        PreparedStatement statement;
+    try {
+        statement = cnx.prepareStatement(
+                "DELETE FROM auction WHERE idAuction = ?");
+        statement.setInt(1, id);
+        statement.executeUpdate();
+	System.out.println("deleted successfully");
+    } catch (SQLException ex) {
+        Logger.getLogger(AuctionDaoImplementation.class.getName()).log(Level.SEVERE, null, ex);
+    }
+        
+
+    }
+
+  @Override
+    public Float getHighestBidById(int id) {
+ try {
+          PreparedStatement statement = cnx.prepareStatement(
+                  "SELECT highestBid FROM auction join bid on auction.idAuction = bid.idAuction WHERE auction.idAuction = ?");     
+        statement.setInt(1, id);
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+       float floatValue = resultSet.getFloat("highestBid");
+        return floatValue;}
+        else {
+            System.out.println("error");
+        }
+       
+
+        
+          
+      } catch (SQLException ex) {
+          Logger.getLogger(BidDaoImplementation.class.getName()).log(Level.SEVERE, null, ex);
+      }
+ return null;
+    }
+
+
+
+}
+
---
+++ Dao/ServicesSpareParts.java
@@ -0,0 +1,166 @@
+/*
+ * //Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * //Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dao;
+//import Entities.SpareParts;
+//import Entities.*;
+//import Entities.SpareParts;
+//import java.sql.*;
+//import Services.*;
+//import Utils.DBconnexion;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//import Utils.*;
+
+import Entities.SpareParts;
+import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import Services.InterfaceSpareParts;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import Utils.DBconnexion;
+
+/*
+import entities.SpareParts;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import interfaces.InterfaceSpareParts;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import tools.MyConnection;
+ */
+/**
+ *
+ * @author Yasmine
+ *
+ * public class ReservationDao implements IReservationDao{
+ *
+ * Connection cnx;
+ *
+ * public ReservationDao()throws SQLException { cnx =
+ * DBconnexion.getInstance().getConnection(); }
+ */
+public class ServicesSpareParts {
+
+    Statement ste;
+
+    Connection cnx;
+
+    public ServicesSpareParts() throws SQLException {
+        cnx = (Connection) DBconnexion.getInstance().getConnection();
+    }
+
+    public List<SpareParts> display() {
+        List<SpareParts> listpieces = new ArrayList<>();
+        try {
+            ste = cnx.createStatement();
+            String req_select = "SELECT * FROM `carbid`.`spareparts`";
+            ResultSet res = ste.executeQuery(req_select);
+            while (res.next()) {
+                int Id = res.getInt(1);
+                String Type = res.getString(2);
+                int Pou = res.getInt(3);
+                String Description = res.getString(4);
+                double Price = res.getDouble(5);
+                String Typec = res.getString(6);
+                //   SpareParts s = new SpareParts(Id,Type,Pou,Description,Price,Typec);
+                SpareParts s = new SpareParts(Id, Type, Pou, Description, Price, Typec);
+                listpieces.add(s);
+
+            }
+        } catch (SQLException ex) {
+            System.out.println("SQLException " + ex.getMessage());
+        }
+
+        return listpieces;
+    }
+
+    public void add(SpareParts u) throws SQLException {
+        PreparedStatement pre = cnx.prepareStatement("INSERT INTO `spareparts`(`id_sparepart`, `Type`, `Pou`, `Description`, `Price`, `Typec`) VALUES (?,?,?,?,?,?)");
+
+        pre.setInt(1, u.getId());
+        pre.setString(2, u.getType());
+        pre.setInt(3, u.getPou());
+        pre.setString(4, u.getDescription());
+        pre.setDouble(5, u.getPrice());
+
+        pre.setString(6, u.getTypec());
+
+        pre.executeUpdate();
+        /*
+           if ((text_live_id.getText().isEmpty()) && (!(txt_main_price.getText().isEmpty())) ) {
+          Alert alert = new Alert(AlertType.WARNING);
+                alert.setTitle("Invalid Input");
+                alert.setHeaderText(null);
+                alert.setContentText("Please enter only a live data.");
+                alert.showAndWait();}}
+         */
+
+    }
+
+    public void delete(int id) {
+
+        try {
+            PreparedStatement pre = cnx.prepareStatement("delete from spareparts where id_sparepart = ?");
+            pre.setInt(1, id);
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.print(ex.getMessage());
+        }
+
+    }
+
+    public void modify(SpareParts u) {
+
+        try {
+            PreparedStatement pre = cnx.prepareStatement("Update spareparts set Type=?,Pou=?,Description=?,Price=?,Typec=? where id_sparepart = ?");
+
+            pre.setInt(6, u.getId());
+
+            pre.setString(1, u.getType());
+
+            pre.setInt(2, u.getPou());
+
+            pre.setString(3, u.getDescription());
+
+            pre.setDouble(4, u.getPrice());
+
+            pre.setString(5, u.getTypec());
+
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.print(ex.getMessage());
+        }
+    }
+
+    public SpareParts GetSparePartsById(int id) {
+
+        try {
+
+            PreparedStatement pre = cnx.prepareStatement("select * from spareparts where id_sparepart = ?");
+
+            pre.setInt(1, id);
+            ResultSet result = pre.executeQuery();
+            while (result.next()) {
+
+                SpareParts u = new SpareParts(result.getInt(1), result.getString(2), result.getInt(3), result.getString(4), result.getDouble(5), result.getString(6));
+                return u;
+            }
+        } catch (SQLException ex) {
+            System.out.print(ex.getMessage());
+        }
+        return null;
+    }
+
+}
