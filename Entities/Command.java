bb3e08113df65077ccc6e92741297713666a80d1
yasminemar <80041494+yasminemar@users.noreply.github.com>
2023-02-25 19:15:29 +0100

    Add files via upload

---
+++ Entities/Command.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entities;
+
+/**
+ *
+ * @author Yasmine
+ */
+public class Command {
+    private int id_command;
+    private int id_user;
+    private int id_sparepart;
+    public Command(int id_command,int id_user,int id_sparepart)
+    {
+        this.id_command=id_command;
+        this.id_user=id_user;
+        this.id_sparepart=id_sparepart;
+    }
+    public Command(){};
+
+    public int getId_command() {
+        return id_command;
+    }
+
+    public int getId_user() {
+        return id_user;
+    }
+
+    public int getId_sparepart() {
+        return id_sparepart;
+    }
+
+    public void setId_command(int id_command) {
+        this.id_command = id_command;
+    }
+
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
+
+    public void setId_sparepart(int id_sparepart) {
+        this.id_sparepart = id_sparepart;
+    }
+
+    @Override
+    public String toString() {
+        return "Command{" + "id_command=" + id_command + ", id_user=" + id_user + ", id_sparepart=" + id_sparepart + '}';
+    }
+    
+    
+    }
+
---
+++ Entities/Reservation.java
@@ -0,0 +1,76 @@
+package Entities;
+
+// import java.sql.*;
+// import java.text.SimpleDateFormat;
+
+/**
+ *
+ * @author neil
+ */
+public class Reservation {
+    private String date;
+    private String location;
+    private int id_user;
+    private int id_car;
+    private int id_agent;
+
+    public Reservation(int id_user, int id_car, String date, String location, int id_agent) {
+        this.date = date;
+        this.location = location;
+        this.id_user = id_user;
+        this.id_car = id_car;
+        this.id_agent = id_agent;
+    }
+
+    // get set for Date attribute
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    // get set for location attribute
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    // get set for id_user attribute
+    public int getUser() {
+        return id_user;
+    }
+
+    public void setUser(int id_user) {
+        this.id_user = id_user;
+    }
+
+    // get set for id_car attribute
+    public int getCar() {
+        return id_car;
+    }
+
+    public void setCar(int id_car) {
+        this.id_car = id_car;
+    }
+
+    // get set for id_agent attribute
+    public int getAgent() {
+        return id_agent;
+    }
+
+    public void setAgent(int id_agent) {
+        this.id_agent = id_agent;
+    }
+
+    @Override
+    public String toString() {
+        return "reservation{" + "user=" + id_user + ", car=" + id_car + ", date=" + date + ", location=" + location
+                + ", agent=" + id_agent + '}';
+    }
+
+}
